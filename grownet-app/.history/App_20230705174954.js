231ddf00287d1a2cd935fc46421debc8802b91c1
Juan Vanegas <59509327+Juan-cyber1@users.noreply.github.com>
2023-07-13 13:39:59 -0500

    Delete grownet-app/.history directory

--- grownet-app/.history/App_20230705174954.js
+++
@@ -1,8 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
-import Navigation from './assets/navigation';
-export default function App() {
-  return (
-    <Navigation/>
-  );
-}
