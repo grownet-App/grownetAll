d15d2029f9f6e32cf20cd06813f2309c317a88aa
liamgabriel2021 <arbey221@gmail.com>
2023-09-07 16:29:07 -0500

    capture and send data and otp to apiOtp

--- grownet-app/screens/otp.js
+++ grownet-app/screens/otp.js
@@ -1,83 +1,107 @@
-import { StyleSheet, Text, TextInput, Image, TouchableOpacity, Alert, View, SafeAreaView } from 'react-native';
-import React, { Component, useRef, useState } from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { GlobalStyles } from './styles'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Alert,
+  View,
+  SafeAreaView,
+} from "react-native";
+import React, { Component, useRef, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { GlobalStyles } from "./styles";
+import { useRoute } from "@react-navigation/native";
+import { otpApiUrl } from "../config/urls.config";
+import axios from "axios";
 
 const otp = () => {
-    const navigation = useNavigation();
-    const pin1Ref = useRef()
-    const pin2Ref = useRef()
-    const pin3Ref = useRef()
-    const pin4Ref = useRef()
-    const [pin1, setPin1] = useState("");
-    const [pin2, setPin2] = useState("");
-    const [pin3, setPin3] = useState("");
-    const [pin4, setPin4] = useState("");
+  const navigation = useNavigation();
+  const pin1Ref = useRef();
+  const pin2Ref = useRef();
+  const pin3Ref = useRef();
+  const pin4Ref = useRef();
+  const [pin1, setPin1] = useState("");
+  const [pin2, setPin2] = useState("");
+  const [pin3, setPin3] = useState("");
+  const [pin4, setPin4] = useState("");
 
+  const route = useRoute();
 
+  const enviarOTP = async () => {
+    let otp = pin1 + pin2 + pin3 + pin4;
 
-    const enviarOTP = () => {
-        let otp = pin1 + pin2 + pin3 + pin4;
-        console.log(otp)
+    const formData = route.params;
+
+    const apiUrl = `${otpApiUrl}?countrie=${formData.countrie}&telephone=${formData.telephone}&code=${otp}`;
+
+    try {
+      const response = await axios.post(apiUrl);
+      console.log("Respuesta de la API:", response.data);
+      const token = response.data.token;
+      console.log("token:", token);
+    } catch (error) {
+      console.error("Error al enviar OTP:", error);
     }
+  };
 
-    return (
-        <SafeAreaView style={GlobalStyles.container}>
-            <View style={GlobalStyles.containerOTP}>
-                <View style={GlobalStyles.textInputView}>
-                    <TextInput
-                        ref={pin1Ref}
-                        style={GlobalStyles.textInput}
-                        keyboardType={'number-pad'}
-                        maxLength={1}
-                        onChangeText={text => {
-                            setPin1(text)
-                            text ? pin2Ref.current.focus() : pin1Ref.current.focus();
-                        }}
-                    />
-                </View>
-                <View style={GlobalStyles.textInputView}>
-                    <TextInput
-                        ref={pin2Ref}
-                        style={GlobalStyles.textInput}
-                        keyboardType={'number-pad'}
-                        maxLength={1}
-                        onChangeText={text => {
-                            setPin2(text)
-                            text ? pin3Ref.current.focus() : pin1Ref.current.focus();
-                        }}
-                    />
-                </View>
-                <View style={GlobalStyles.textInputView}>
-                    <TextInput
-                        ref={pin3Ref}
-                        style={GlobalStyles.textInput}
-                        keyboardType={'number-pad'}
-                        maxLength={1}
-                        onChangeText={text => {
-                            setPin3(text)
-                            text ? pin4Ref.current.focus() : pin2Ref.current.focus();
-                        }}
-                    />
-                </View>
-                <View style={GlobalStyles.textInputView}>
-                    <TextInput
-                        ref={pin4Ref}
-                        style={GlobalStyles.textInput}
-                        keyboardType={'number-pad'}
-                        maxLength={1}
-                        onChangeText={text => {
-                            setPin4(text)
-                            !text && pin3Ref.current.focus();
-                        }}
-                    />
-                </View>
-            </View>
-            <TouchableOpacity onPress={enviarOTP}><Text>Verificar</Text></TouchableOpacity>
-        </SafeAreaView>
-
-    )
-
-}
+  return (
+    <SafeAreaView style={GlobalStyles.container}>
+      <View style={GlobalStyles.containerOTP}>
+        <View style={GlobalStyles.textInputView}>
+          <TextInput
+            ref={pin1Ref}
+            style={GlobalStyles.textInput}
+            keyboardType={"number-pad"}
+            maxLength={1}
+            onChangeText={(text) => {
+              setPin1(text);
+              text ? pin2Ref.current.focus() : pin1Ref.current.focus();
+            }}
+          />
+        </View>
+        <View style={GlobalStyles.textInputView}>
+          <TextInput
+            ref={pin2Ref}
+            style={GlobalStyles.textInput}
+            keyboardType={"number-pad"}
+            maxLength={1}
+            onChangeText={(text) => {
+              setPin2(text);
+              text ? pin3Ref.current.focus() : pin1Ref.current.focus();
+            }}
+          />
+        </View>
+        <View style={GlobalStyles.textInputView}>
+          <TextInput
+            ref={pin3Ref}
+            style={GlobalStyles.textInput}
+            keyboardType={"number-pad"}
+            maxLength={1}
+            onChangeText={(text) => {
+              setPin3(text);
+              text ? pin4Ref.current.focus() : pin2Ref.current.focus();
+            }}
+          />
+        </View>
+        <View style={GlobalStyles.textInputView}>
+          <TextInput
+            ref={pin4Ref}
+            style={GlobalStyles.textInput}
+            keyboardType={"number-pad"}
+            maxLength={1}
+            onChangeText={(text) => {
+              setPin4(text);
+              !text && pin3Ref.current.focus();
+            }}
+          />
+        </View>
+      </View>
+      <TouchableOpacity onPress={enviarOTP}>
+        <Text>Verificar</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
 
 export default otp;
