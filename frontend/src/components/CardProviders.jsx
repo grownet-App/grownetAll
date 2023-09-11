846a0698ca93d6715ecd6bb26cabb6047bf991d2
Julian Celeita <andresceleita@yahoo.es>
2023-09-11 17:08:30 -0500

    API de providers conectada a la app

--- frontend/src/components/CardProviders.jsx
+++ frontend/src/components/CardProviders.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 import img_providers from "../img/img_providers.png";
-import logo__foodpoint from "../img/logo__foodpoint.svg";
+import axios from "axios";
+import { availableProviders } from "../config/urls.config";
 
-export default function Providers(props) {
+export default function Providers() {
+  const [providers, setProviders] = useState([]);
+  useEffect(() => {
+    axios
+      .get(availableProviders)
+      .then((response) => {
+        setProviders(response.data.providers);
+        console.log(response.data);
+        console.log("THIS IS THE PROVIDER", providers);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los proveedores:", error);
+      });
+  }, []);
 
-    const [providers, setProviders] = useState([]);
-
-        useEffect (() => {
-            fetch ("http://5.161.211.8:88/api/providers/" + "all")
-            .then ((response) => response.json ())
-            .then (( {providers}) => setProviders(providers));
-        }, [ ]);
-              
-    return (
-        <section className='providers'>
-           {/*
-            <div>
-            <ul>
-            {providers.map (providers => <p key={providers.id}>{providers.name}</p>)}
-            </ul>
-            </div>*/
-           }
-           
-            <a className="bttn-categ" id='providers-categ' href='/products'>
-            {providers.filter(sup => sup.id === props.prove).map(filteredPerson => (
-             <h2 className='text-supplier'>
-            {filteredPerson.name}
-            </h2>))}
-                <img src={img_providers} alt="logo-restaurant" className='img-providers'></img>
-            </a>
-           
-
- </section>
-    );
+  return (
+    <section className="providers">
+      {providers.map((provider) => (
+        <a className="bttn-categ" id="providers-categ" href="/products">
+          <h2 className="text-supplier">{provider.name}</h2>
+          <img
+            src={img_providers}
+            alt="logo-restaurant"
+            className="img-providers"
+          ></img>
+        </a>
+      ))}
+    </section>
+  );
 }
--- frontend/src/pages/BuyProcess/Providers.jsx
+++ frontend/src/pages/BuyProcess/Providers.jsx
@@ -1,25 +1,10 @@
 import { Icon } from "@iconify/react";
-import React, {useEffect, useState} from "react";
+import React from "react";
 import CardProviders from "../../components/CardProviders.jsx";
 import Menu from "../../components/Menu/Menu.jsx";
-import "../../css/providers.css"
-import axios from "axios";
-import { availableProviders } from "../../config/urls.config";
+import "../../css/providers.css";
 
 export default function Providers() {
-  const urlImg = "https://ec2-18-191-177-149.us-east-2.compute.amazonaws.com/grownet/";
-  const [providers, setProviders] = useState([]);
-
-useEffect (() => {
-  axios.get(availableProviders).then((response) => {
-    setProviders(response.data.customersChef);
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.error('Error al obtener los proveedores:', error);
-  });
-}, []);
-
   return (
     <section className="providers">
       <div className="tittle-providers">
@@ -33,11 +18,12 @@ useEffect (() => {
         </a>
         <h1 className="tittle-restaurants">Providers</h1>
       </div>
-      <CardProviders prove={1}></CardProviders>
-      <CardProviders prove={2}></CardProviders>
-      <CardProviders prove={3}></CardProviders>
-      <CardProviders prove={4}></CardProviders>
-      <a className="bttn btn-primary" href="settingsProviders"id="bttn-restaurant">
+      <CardProviders></CardProviders>
+      <a
+        className="bttn btn-primary"
+        href="settingsProviders"
+        id="bttn-restaurant"
+      >
         <Icon className="icon-plus" icon="simple-line-icons:plus" />
         Add providers
       </a>
