c8555e2ff66aaca8b8010640d4000777815e201f
Julian Celeita <andresceleita@yahoo.es>
2023-09-18 17:50:16 -0500

    TODO en restaurants, providers y cardsuppliers

--- frontend/src/pages/BuyProcess/Restaurants.jsx
+++ frontend/src/pages/BuyProcess/Restaurants.jsx
@@ -4,12 +4,11 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { availableRestaurants } from "../../config/urls.config";
 import "../../css/restaurants.css";
+import backgroundRestaurants from "../../img/backgroundRestaurants.png";
 import useOrderStore from "../../store/useOrderStore";
 import useTokenStore from "../../store/useTokenStore";
-import backgroundRestaurants from "../../img/backgroundRestaurants.png";
 
 export default function Restaurants() {
-  //TODO QUITAR ESTA URL CUANDO EL BACKEND HAYA CARGADO EL LINK DE LA IMG
   const urlImg =
     "https://ec2-18-191-177-149.us-east-2.compute.amazonaws.com/grownet/";
   const { token } = useTokenStore();
@@ -36,27 +35,23 @@ export default function Restaurants() {
     setSelectedRestaurant(restaurant);
   };
 
-  //TODO ELIMINAR ESTE CONSOLE LOG DE DATOS DEL RESTAURANTE
-  console.log("RESTAURANTES:", restaurants);
-
   return (
     <section className="restaurants">
       <h1 className="tittle-restaurants">Choose your restaurant</h1>
-      
+
       {restaurants.map((restaurant) => (
-        <Link id="background-boton"
-          className="bttn-categ" style={{backgroundImage: `url(${backgroundRestaurants})` }}
+        <Link
+          id="background-boton"
+          className="bttn-categ"
+          style={{ backgroundImage: `url(${backgroundRestaurants})` }}
           onClick={() => handleRestaurantSelect(restaurant)}
           to={"/suppliers"}
           key={restaurant.account_number}
         >
-          
-          <img src={urlImg + restaurant.image} alt={restaurant.account_name}  />
+          <img src={urlImg + restaurant.image} alt={restaurant.account_name} />
           <div className="text-categ" key={restaurant.account_number}>
-            {/*<h2>{restaurant.account_name}</h2>*/}
             <p>{restaurant.address}</p>
           </div>
-          
         </Link>
       ))}
       <Link
--- frontend/src/pages/BuyProcess/Suppliers.jsx
+++ frontend/src/pages/BuyProcess/Suppliers.jsx
@@ -1,19 +1,16 @@
 import { Icon } from "@iconify/react";
 import React from "react";
+import { Link } from "react-router-dom";
 import CardSuppliers from "../../components/CardSuppliers.jsx";
 import Menu from "../../components/Menu/Menu.jsx";
 import "../../css/suppliers.css";
-import { Link } from "react-router-dom";
+
 export default function Suppliers() {
   return (
     <section className="suppliers">
       <div className="tittle-suppliers">
         <Link to="/restaurants">
-          <Icon
-            src="google.com"
-            icon="ic:round-arrow-back"
-            className="arrow"
-          />
+          <Icon src="google.com" icon="ic:round-arrow-back" className="arrow" />
         </Link>
         <h1 className="tittle-restaurants">Suppliers</h1>
       </div>
