8909dd555641b4365d4d302610f5ce64c5d49314
pvgmoon <pandreavanegasg@gmail.com>
2023-10-11 08:45:22 -0500

    Commit para pull

--- grownet-app/App.js
+++ grownet-app/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navigation from './src/navigation/navigation'
+import Navigation from './src/navigation/Navigation'
 
 export default function App() {
   return <Navigation />
