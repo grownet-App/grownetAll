213e910872b556fb92032c8d07467cc79a86a154
Julian Celeita <andresceleita@yahoo.es>
2023-10-24 13:42:26 -0500

    Ajuste de estilos para página de Intercom

--- frontend/src/pages/ChatIntercom.jsx
+++ frontend/src/pages/ChatIntercom.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import img_succesful from '../img/img_succesful.png';
 
 const ChatIntercom = () => {
+    const { t } = useTranslation();
     useEffect(() => {
         // Asegurarse de que Intercom esté cargado y abierto cuando el componente se monta
         if (window.Intercom) {
@@ -9,14 +13,21 @@ const ChatIntercom = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Bienvenido al soporte de Grownet</h1>
-            <p>Haz clic en el chat en la esquina inferior para empezar a conversar con nosotros.</p>
-            {/* Si deseas un botón personalizado para abrir el chat: */}
-            <button id="my-intercom" onClick={() => window.Intercom('show')}>
-                Abrir chat de soporte
-            </button>
-        </div>
+        <section className="order-succesful">
+      <img src={img_succesful} alt="Chat Image" />
+      <h1>{/* {t("orderSuccessful.title")} */} Bienvenido al soporte de Grownet</h1>
+      <p>{/* {t("orderSuccessful.message")} */} Un agente está del otro lado esperando hablar contigo.</p>
+      <div className="buttons-succesful">
+        
+        <button
+          className="bttn btn-outline"
+          id="my-intercom"
+          onClick={() => window.Intercom('show')}
+        >
+          {/* {t("orderSuccessful.pdfButton")} */} Abrir chat de soporte
+        </button>
+      </div>
+    </section>
     );
 }
 
