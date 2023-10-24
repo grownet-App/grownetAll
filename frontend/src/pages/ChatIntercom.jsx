d596eb335f34582f2aa94d2cdc1d57aa48c38b75
Julian Celeita <andresceleita@yahoo.es>
2023-10-24 18:24:53 -0500

    Prueba para registro de usuario intercom

--- frontend/src/pages/ChatIntercom.jsx
+++ frontend/src/pages/ChatIntercom.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import img_succesful from "../img/img_succesful.png";
 import "../css/chatIntercom.css";
+import { useEffect } from "react";
 
 const ChatIntercom = () => {
-const urlParams = new URLSearchParams(window.location.search);
-const userName = urlParams.get('name');
+  const urlParams = new URLSearchParams(window.location.search);
+  const userName = urlParams.get("name");
+
+  useEffect(() => {
+    if (window.Intercom && userName) {
+        window.Intercom("boot", {
+            app_id: "pqsj7h24",
+            name: userName,
+        });
+    }
+}, [userName]);
 
   return (
     <section className="chat-intercom">
       <img src={img_succesful} alt="Chat grownet" />
       <h1>Welcome to Grownet Support</h1>
-      <p>An agent is waiting to speak to you.</p>
-        <p>Thank you Mr. {userName}. </p>
+      <p>{userName} an agent is waiting to speak to you.</p>
       <div className="buttons-succesful">
         <button className="bttn btn-outline" id="my-intercom">
           Open chat
