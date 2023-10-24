bfbb903365b7f1e91af605571ef7f8242ad5f8b2
Julian Celeita <andresceleita@yahoo.es>
2023-10-24 15:24:37 -0500

    Se pule la pagina de Intercom

--- frontend/src/pages/ChatIntercom.jsx
+++ frontend/src/pages/ChatIntercom.jsx
@@ -1,34 +1,20 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import img_succesful from '../img/img_succesful.png';
+import React from "react";
+import img_succesful from "../img/img_succesful.png";
 
 const ChatIntercom = () => {
-    const { t } = useTranslation();
-    useEffect(() => {
-        // Asegurarse de que Intercom esté cargado y abierto cuando el componente se monta
-        if (window.Intercom) {
-            window.Intercom('show');
-        }
-    }, []);
 
-    return (
-        <section className="order-succesful">
-      <img src={img_succesful} alt="Chat Image" />
-      <h1>{/* {t("orderSuccessful.title")} */} Bienvenido al soporte de Grownet</h1>
-      <p>{/* {t("orderSuccessful.message")} */} Un agente está del otro lado esperando hablar contigo.</p>
+  return (
+    <section className="order-succesful">
+      <img src={img_succesful} alt="Chat grownet" />
+      <h1>Welcome to Grownet Support</h1>
+      <p>An agent is on the other side waiting to talk to you.</p>
       <div className="buttons-succesful">
-        
-        <button
-          className="bttn btn-outline"
-          id="my-intercom"
-          onClick={() => window.Intercom('show')}
-        >
-          {/* {t("orderSuccessful.pdfButton")} */} Abrir chat de soporte
+        <button className="bttn btn-outline" id="my-intercom">
+          Open chat
         </button>
       </div>
     </section>
-    );
-}
+  );
+};
 
 export default ChatIntercom;
