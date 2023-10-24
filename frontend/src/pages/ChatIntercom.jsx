82a9ee99105be833ab7ea33f9852965201f127c9
Julian Celeita <andresceleita@yahoo.es>
2023-10-24 16:02:04 -0500

    Arreglo de estilos del chat

--- frontend/src/pages/ChatIntercom.jsx
+++ frontend/src/pages/ChatIntercom.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import img_succesful from "../img/img_succesful.png";
+import "../css/chatIntercom.css";
 
 const ChatIntercom = () => {
 
   return (
-    <section className="order-succesful">
+    <section className="chat-intercom">
       <img src={img_succesful} alt="Chat grownet" />
       <h1>Welcome to Grownet Support</h1>
-      <p>An agent is on the other side waiting to talk to you.</p>
+      <p>An agent is waiting to speak to you.</p>
       <div className="buttons-succesful">
         <button className="bttn btn-outline" id="my-intercom">
           Open chat
