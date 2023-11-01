eacba4a9b1eb173f4c919afb0f5fc4985a6b8451
Julian Celeita <andresceleita@yahoo.es>
2023-11-01 13:48:52 -0500

    Las ordenes ya se separan entre closed pending

--- grownet-app/src/screens/Record/Records.jsx
+++ grownet-app/src/screens/Record/Records.jsx
@@ -18,19 +18,23 @@ import { GlobalStyles } from '../../styles/Styles'
 const Records = ({ navigation }) => {
   const { t } = useTranslation()
   const { token } = useTokenStore()
+  const {
+    pendingOrders,
+    closedOrders,
+    setPendingOrders,
+    setClosedOrders,
+    setSelectedPendingOrder,
+    setSelectedClosedOrder,
+  } = useRecordStore()
   const { selectedRestaurant } = useOrderStore()
-  const { pendingOrders, setPendingOrders, setSelectedPendingOrder } =
-    useRecordStore()
   const apiOrders = allStorageOrders + selectedRestaurant.accountNumber
   const [activeTab, setActiveTab] = useState('pendingRecord')
-  console.log(apiOrders)
   const switchTab = () => {
     setActiveTab((prevTab) =>
       prevTab === 'pastRecord' ? 'pendingRecord' : 'pastRecord',
     )
   }
   useEffect(() => {
-    // LLAMAR LAS ORDENES PENDIENTES DE LA BASE DE DATOS
     if (selectedRestaurant === null) {
       navigation.navigate('restaurants')
       return
@@ -42,11 +46,16 @@ const Records = ({ navigation }) => {
         },
       })
       .then((response) => {
-        setPendingOrders(
-          response.data.orders.map((order) => ({
-            ...order,
-          })),
+        const closedOrders = response.data.orders.filter(
+          (order) => order.id_stateOrders === 5,
         )
+        const pendingOrders = response.data.orders.filter(
+          (order) => order.id_stateOrders !== 5,
+        )
+        console.log('Closed orders:', closedOrders)
+        console.log('Pending orders:', pendingOrders)
+        setClosedOrders(closedOrders)
+        setPendingOrders(pendingOrders)
       })
       .catch((error) => {
         console.log('Error al llamar las ordenes', error)
@@ -86,7 +95,8 @@ const Records = ({ navigation }) => {
   return (
     <SafeAreaView style={RecordStyle.record}>
       <ScrollView>
-        {pendingOrders.length === 0 ? (
+        {/* AVISO DE QUE NO HAY ORDENES */}
+        {pendingOrders.length === 0 && closedOrders.length === 0 ? (
           <View style={RecordStyle.recordZero}>
             <Image source={require('../../../assets/img/img-succesful.png')} />
             <Text style={RecordStyle.textZero}>{t('record.noOrders')}</Text>
@@ -101,6 +111,7 @@ const Records = ({ navigation }) => {
           </View>
         ) : (
           <>
+            {/* FILTRO POR FECHA */}
             <View style={RecordStyle.filter}>
               <Button onPress={showDatepicker}>
                 <Text style={RecordStyle.textFilter}>{formattedDate}</Text>
@@ -120,6 +131,7 @@ const Records = ({ navigation }) => {
               )}
             </View>
             <View style={[RecordStyle.tabContainer, GlobalStyles.boxShadow]}>
+              {/* SWITCH ENTRE PASTORDERS Y PENDINGORDERS */}
               <TouchableOpacity
                 style={[
                   {
@@ -166,34 +178,142 @@ const Records = ({ navigation }) => {
             </View>
 
             <View>
+              {/* VISTA SI ESTÁ PASTRECORD SELECCIONADO */}
               {activeTab === 'pastRecord' ? (
-                <View style={[RecordStyle.cardRecord, GlobalStyles.boxShadow]}>
-                  <View style={RecordStyle.textCard}>
-                    <Text style={RecordStyle.tittle}> {t('record.order')}</Text>
-                    <Text style={RecordStyle.text}>11154</Text>
-                    <Text style={RecordStyle.tittle}>
-                      {' '}
-                      {t('record.amount')}
-                    </Text>
-                    <Text style={RecordStyle.text}>£856</Text>
-                  </View>
-                  <View style={RecordStyle.textCard}>
-                    <Text style={RecordStyle.tittle}>{t('record.date')}</Text>
-                    <Text style={RecordStyle.text}>01/10/2023</Text>
+                <View>
+                  {formattedDate === 'All orders'
+                    ? closedOrders.map((order) => (
+                        <View
+                          style={[
+                            RecordStyle.cardRecord,
+                            GlobalStyles.boxShadow,
+                          ]}
+                          key={order.reference}
+                        >
+                          <View
+                            style={RecordStyle.textCard}
+                            key={order.reference}
+                          >
+                            <Text style={RecordStyle.tittle}>
+                              {' '}
+                              {t('record.order')}
+                            </Text>
+                            <Text style={RecordStyle.text}>
+                              {order.reference}
+                            </Text>
+                            <Text style={RecordStyle.tittle}>
+                              {t('record.amount')}
+                            </Text>
+                            <Text style={RecordStyle.text}>£{order.total}</Text>
+                          </View>
+                          <View style={RecordStyle.textCard}>
+                            <Text style={RecordStyle.tittle}>
+                              {t('record.date')}
+                            </Text>
+                            <Text style={RecordStyle.text}>
+                              {order.date_delivery}
+                            </Text>
+                            <Button
+                              title="View details"
+                              style={RecordStyle.btnPrimary}
+                              onPress={() =>
+                                navigation.navigate('pendingRecord')
+                              }
+                            >
+                              <Text style={GlobalStyles.textBtnSecundary}>
+                                {t('record.viewDetails')}
+                              </Text>
+                            </Button>
+                          </View>
+                        </View>
+                      ))
+                    : closedOrders
+                        .filter((order) => {
+                          const orderDate = new Date(order.date_delivery)
+                          const selectedDateUTC = new Date(
+                            selectedDate.toUTCString(),
+                          )
 
-                    <Button
-                      title="View details"
-                      style={RecordStyle.btnPrimary}
-                      onPress={() => handlePendingOrderSelect(order.reference)}
-                    >
-                      <Text style={GlobalStyles.textBtnSecundary}>
-                        {t('record.viewDetails')}
-                      </Text>
-                    </Button>
-                  </View>
+                          return (
+                            orderDate.getUTCDate() ===
+                              selectedDateUTC.getUTCDate() &&
+                            orderDate.getUTCMonth() ===
+                              selectedDateUTC.getUTCMonth() &&
+                            orderDate.getUTCFullYear() ===
+                              selectedDateUTC.getUTCFullYear()
+                          )
+                        })
+                        .map((order) => (
+                          <View
+                            style={[
+                              RecordStyle.cardRecord,
+                              GlobalStyles.boxShadow,
+                            ]}
+                            key={order.reference}
+                          >
+                            <View
+                              style={RecordStyle.textCard}
+                              key={order.reference}
+                            >
+                              <Text style={RecordStyle.tittle}>
+                                {' '}
+                                {t('record.order')}
+                              </Text>
+                              <Text style={RecordStyle.text}>
+                                {order.reference}
+                              </Text>
+                              <Text style={RecordStyle.tittle}>
+                                {t('record.amount')}
+                              </Text>
+                              <Text style={RecordStyle.text}>
+                                £{order.total}
+                              </Text>
+                            </View>
+                            <View style={RecordStyle.textCard}>
+                              <Text style={RecordStyle.tittle}>
+                                {t('record.date')}
+                              </Text>
+                              <Text style={RecordStyle.text}>
+                                {order.date_delivery}
+                              </Text>
+                              <Button
+                                title="View details"
+                                style={RecordStyle.btnPrimary}
+                                onPress={() =>
+                                  navigation.navigate('pendingRecord')
+                                }
+                              >
+                                <Text style={GlobalStyles.textBtnSecundary}>
+                                  {t('record.viewDetails')}
+                                </Text>
+                              </Button>
+                            </View>
+                          </View>
+                        ))}
+                  {selectedDate &&
+                    formattedDate !== 'All orders' &&
+                    !closedOrders.some((order) => {
+                      const orderDate = parseISO(order.date_delivery)
+                      return isSameDay(orderDate, selectedDate)
+                    }) && (
+                      <View style={RecordStyle.dateZero}>
+                        <MaterialIcons
+                          name="error-outline"
+                          size={100}
+                          color="#026CD2"
+                        />
+                        <Text style={RecordStyle.textDateZero}>
+                          {t('record.noOrdersDate')}
+                        </Text>
+                        <Text style={RecordStyle.textDateFilter}>
+                          {formattedDate}
+                        </Text>
+                      </View>
+                    )}
                 </View>
               ) : (
                 <View>
+                  {/* VISTA SI ESTÁ PENDINGRECORD SELECCIONADO */}
                   {formattedDate === 'All orders' ? (
                     <View>
                       {pendingOrders.map((order) => (
@@ -202,6 +322,7 @@ const Records = ({ navigation }) => {
                             RecordStyle.cardRecord,
                             GlobalStyles.boxShadow,
                           ]}
+                          key={order.reference}
                         >
                           <View
                             style={RecordStyle.textCard}
@@ -265,6 +386,7 @@ const Records = ({ navigation }) => {
                               RecordStyle.cardRecord,
                               GlobalStyles.boxShadow,
                             ]}
+                            key={order.reference}
                           >
                             <View
                               style={RecordStyle.textCard}
